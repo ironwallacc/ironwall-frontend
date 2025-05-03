e0bd104e43054f9779a25cc1862b270928571fc2
ironwallacc <your-email@example.com>
2025-05-03 07:39:33 +0100

    fix: add LF + exec permission to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Exit immediately if any command fails
+set -e
+
+# Install Vite globally
 npm install -g vite@latest
+
+# Install project dependencies
+npm install
+
+# Build with Vite
 vite build
